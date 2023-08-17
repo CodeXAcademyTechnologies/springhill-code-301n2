f4f79e676d60470cb94995d04991e427999533b1
Elias <elias.staehle@gmail.com>
2023-08-16 21:27:30 -0400

    added Class-01 HornedBeast demo

---
+++ class-01/springhill-301n2-HornedBeasts/src/Gallery.jsx
@@ -0,0 +1,16 @@
+import { Component } from 'react'
+
+export default class Gallery extends Component {
+
+  render() {
+
+
+    return (
+      <>
+        <div>Gallery</div>
+        <HornedBeast title={'unicorn'} imageUrl={'https://images.unsplash.com/photo-1617001390418-d38e36870069?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80'} description={'a horned horse'} />
+        <HornedBeast title={ } imageUrl={ } description={ } />
+      </>
+    )
+  }
+}
